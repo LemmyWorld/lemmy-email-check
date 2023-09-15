115218e0f9607c39e7b4cc4836d18b7fd3932fc2
Jesse Schoepfer <jelloeater@gmail.com>
2023-09-14 23:24:18 -0400

    Fix: added missing list and refactored to classes

--- emailchecker/fetchLists.py
+++ emailchecker/fetchLists.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def run():
     email_lists = []
 
@@ -14,10 +15,9 @@ def run():
         r = requests.get(url)
         for email in r.text.splitlines():
             if email.strip() != "" and not email.strip() in disposable_emails:
-                print("Adding " + email.strip()) # FIXME change this to a debug logging statement
+                print("Adding " + email.strip())  # FIXME change this to a debug logging statement
                 disposable_emails.append(email.strip())
         print("Done Fetching")
-
 
     with open('./manual_blocklist.list', 'r') as file:
         for email in file:
@@ -28,3 +28,7 @@ def run():
         file.truncate(0)
         for email in disposable_emails:
             file.write(email + "\n")
+
+
+if __name__ == "__main__":
+    run()
